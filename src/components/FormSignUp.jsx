01e4bff1f3510e49d49aa8ecc76cf0872f41528e
Omar Augusto Bautista Mora <oabm77@gmail.com>
2023-08-30 01:10:21 -0500

    01 Iniciando con functions components - Material UI

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,7 +1,18 @@
+import Button from "@mui/material/Button"
+
 function FormSignUp() {
     return <form>
+        <label>Nombre</label>
         <input type="text" />
-        <button>Registrarse</button>
+        <label>Apellido</label>
+        <input type="text" />
+        <label>Correo electrónico</label>
+        <input type="text" />
+        <label>Promociones</label>
+        <input type="checkbox" />
+        <label>Novedades</label>
+        <input type="checkbox" />
+        <Button variant="contained">Registrarse</Button>
     </form>
 }
 
