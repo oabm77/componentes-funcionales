f0c006fea6ddce6553e7b9cc6fc50f83ca25913a
Omar Augusto Bautista Mora <oabm77@gmail.com>
2023-08-30 00:40:44 -0500

    01 Iniciando con functions components - Clases vs Function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import ClassComponent from './components/ClassComponent';
+import FuncComponent from './components/FuncComponent';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <ClassComponent />
+      <FuncComponent />
     </div>
   );
 }
